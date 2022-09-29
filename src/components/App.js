0f9a039749ba9f9a7244f09175ef483c5b64b414
Bartek Karge <bob-ziomal@MacBook-Pro-Bartek.local>
2022-09-29 19:00:32 +0200

    changes applied

---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import Footer from "./Footer";
+import axios from "axios";
+import sass from "../styles/style.scss";
+import dice from "../project_info/images/icon-dice.svg";
+
+const App = () => {
+  const [advice, setAdvice] = useState(null);
+  const [adviceNumber, setAdviceNumber] = useState(null);
+
+  const getData = async () => {
+    const { data } = await axios.get("https://api.adviceslip.com/advice");
+    setAdvice(data.slip.advice);
+    setAdviceNumber(data.slip.id);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  let number;
+
+  const parseNumber = () => {
+    if (adviceNumber < 10) {
+      return (number = `00${adviceNumber}`);
+    } else if (adviceNumber > 10 && adviceNumber < 100) {
+      return (number = `0${adviceNumber}`);
+    } else return (number = adviceNumber);
+  };
+  // returnNumber();
+
+  return (
+    <div className="container">
+      <div className="advice__container">
+        <h5 className="advice-title">
+          Advice <span className="advice-number">#{parseNumber()}</span>
+        </h5>
+        <h1 className="advice-content">“{advice}”</h1>
+        <div className="divider__container"></div>
+        <a className="get-advice-btn" onClick={() => getData()}>
+          <img src={dice} alt="dice" className="dice" />
+        </a>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/project_info/style-guide.md
@@ -0,0 +1,21 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+## Typography
+
+### Body Copy
+
+- Font size (quote): 28px
+
+### Font
+
+- Family: [Manrope](https://fonts.google.com/specimen/Manrope)
+- Weights: 800
